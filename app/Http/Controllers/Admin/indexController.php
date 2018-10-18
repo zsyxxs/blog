095c8b8d4ced07eda373b09b130218b935ec0de1
zsy <412272963@qq.com>
2018-10-18 14:35:22 +0800

    新增

---
+++ app/Http/Controllers/Admin/indexController.php
@@ -0,0 +1,12 @@
+<?php
+namespace app\Http\Controllers\Admin;
+
+use Illuminate\Routing\Controller;
+
+class indexController extends Controller
+{
+    public function index()
+    {
+        return '123';
+    }
+}
